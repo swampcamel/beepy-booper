4dc22e6df9ab4a7e742afc9678ee9850ee46cf4a
Dan Mace <dmacebeta@gmail.com>
2018-08-17 09:36:04 -0700

    finished branching functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 $(function () {
-
+// This numCheck function satisfies the requirements of the project.  It displays an understanding of branches, operators and RegEx, but this week was about Arrays, and while splitting the number into individual digits and then checking each digit for the 0 || 1 condition is tempting, the code below is far better performant.  So, I'll hold on to this function cause I like it's simplicity, but to make sure you know I can do the array thing, I'll still dump the values into an array and print them back out to the user.  A back-end window, if you will.
   var numCheck = function(number) {
     var returnedValue = "";
     if (number%3 === 0){
-      return = "I'm sorry, Dave, "
+      returnedValue = "I'm sorry, Dave. I'm afraid I can't do that.";
     } else if (/1.*/.test(number)) {
       returnedValue = "BOOP";
     } else if (/0.*/.test(number)) {
