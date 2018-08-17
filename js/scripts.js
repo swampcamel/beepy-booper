5ed95d4ce907802db200a57715740257733e8746
Dan Mace <dmacebeta@gmail.com>
2018-08-17 14:39:56 -0700

    add sorry dave sound

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,19 @@
 $(function () {
-// This numCheck function satisfies the requirements of the project.  It displays an understanding of branches, operators and RegEx, but this week was about Arrays, and while splitting the number into individual digits and then checking each digit for the 0 || 1 condition is tempting, the code below is far better performant.  So, I'll hold on to this function cause I like it's simplicity, but to make sure you know I can do the array thing, I'll still dump the values into an array and print them back out to the user.  A back-end window, if you will.
+// This numCheck function satisfies the requirements of the project.  It displays an understanding of branches, operators and RegEx, but this week was about Arrays, and while splitting the number into individual digits and then checking each digit for the 0 || 1 condition is tempting, the code below is far better performant.  So, I'll hold on to this function cause I like it's simplicity, but to make sure you know I can do the array thing, I'll still dump the values into an array and print them back out to the user.
   var memoryBanks = [];
   var beep = new Audio('media/bleep_01.wav');
   var boop = new Audio('media/bleep_05.wav');
+  var sorry = new Audio('media/cantdo.wav');
 
   var numCheck = function(number) {
     var returnedValue = "";
     if (number === 0) {
-    returnedValue = "I'm sorry, Dave. I'm afraid I can't do that. You cannot divide the infinite nothing.";
+      sorry.play();
+      returnedValue = "I'm sorry, Dave. I'm afraid I can't do that. You cannot divide the infinite nothing.";
     } else if (number%3 === 0){
+      sorry.play();
       returnedValue = "I'm sorry, Dave. I'm afraid I can't do that. I have not been programmed to process multiples of three.";
+
     } else if (/1.*/.test(number)) {
       $("#title").addClass("shake-horizontal");
       setTimeout(function() {
@@ -17,6 +21,7 @@ $(function () {
       }, 350);
       beep.play();
       returnedValue = "BOOP";
+
     } else if (/0.*/.test(number)) {
       $("#title").addClass("shake-horizontal");
       setTimeout(function() {
@@ -24,6 +29,7 @@ $(function () {
       }, 350);
       boop.play();
       returnedValue = "BEEP";
+
     } else {
       returnedValue = number.toString();
     }
@@ -38,6 +44,7 @@ $(function () {
     var animator = 0;
     var speed = 20;
     var userInput = parseInt($("input#userInput").val());
+
     memoryBanks.push(userInput);
     output = numCheck(userInput);
 
@@ -53,13 +60,14 @@ $(function () {
 
   $("button#full-print").click(function() {
     var total= 0;
+
     memoryBanks.forEach(function(value) {
       total = total + parseInt(value);
     });
+
     var printMemory = memoryBanks.join(" ");
+
     $("#output").html("<p>" + printMemory + "</p>");
     $("#output").append("<p> Total: " + total.toString() + "</p>");
   });
-
-
 });
--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
 _https://www.deviantart.com/x11kjm/art/Retro-Wallpaper-212491997_
 https://creativecommons.org/licenses/by-sa/3.0/
-
+https://www.freespecialeffects.co.uk/pages/computers.html
+http://www.rosswalker.co.uk/movie_sounds/2001_and_2010.htm
 http://animista.net
