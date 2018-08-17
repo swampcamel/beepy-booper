2704b5786427dfe1c05c69becfb6ebe3a39cabd9
Dan Mace <dmacebeta@gmail.com>
2018-08-17 14:31:07 -0700

    added sound and shake animation events for beep and boop

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 $(function () {
 // This numCheck function satisfies the requirements of the project.  It displays an understanding of branches, operators and RegEx, but this week was about Arrays, and while splitting the number into individual digits and then checking each digit for the 0 || 1 condition is tempting, the code below is far better performant.  So, I'll hold on to this function cause I like it's simplicity, but to make sure you know I can do the array thing, I'll still dump the values into an array and print them back out to the user.  A back-end window, if you will.
   var memoryBanks = [];
+  var beep = new Audio('media/bleep_01.wav');
+  var boop = new Audio('media/bleep_05.wav');
 
   var numCheck = function(number) {
     var returnedValue = "";
@@ -9,8 +11,18 @@ $(function () {
     } else if (number%3 === 0){
       returnedValue = "I'm sorry, Dave. I'm afraid I can't do that. I have not been programmed to process multiples of three.";
     } else if (/1.*/.test(number)) {
+      $("#title").addClass("shake-horizontal");
+      setTimeout(function() {
+        $("#title").removeClass("shake-horizontal");
+      }, 350);
+      beep.play();
       returnedValue = "BOOP";
     } else if (/0.*/.test(number)) {
+      $("#title").addClass("shake-horizontal");
+      setTimeout(function() {
+        $("#title").removeClass("shake-horizontal");
+      }, 350);
+      boop.play();
       returnedValue = "BEEP";
     } else {
       returnedValue = number.toString();
@@ -48,5 +60,6 @@ $(function () {
     $("#output").html("<p>" + printMemory + "</p>");
     $("#output").append("<p> Total: " + total.toString() + "</p>");
   });
+
 
 });
--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
 _https://www.deviantart.com/x11kjm/art/Retro-Wallpaper-212491997_
 https://creativecommons.org/licenses/by-sa/3.0/
+
+http://animista.net
