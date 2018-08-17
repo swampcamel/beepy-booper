4006d9552ca98f5414d8be739135c8b176db911e
Dan Mace <dmacebeta@gmail.com>
2018-08-17 14:01:35 -0700

    create storage array that can be repeated back and added together

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 $(function () {
 // This numCheck function satisfies the requirements of the project.  It displays an understanding of branches, operators and RegEx, but this week was about Arrays, and while splitting the number into individual digits and then checking each digit for the 0 || 1 condition is tempting, the code below is far better performant.  So, I'll hold on to this function cause I like it's simplicity, but to make sure you know I can do the array thing, I'll still dump the values into an array and print them back out to the user.  A back-end window, if you will.
+  var memoryBanks = [];
+
   var numCheck = function(number) {
     var returnedValue = "";
     if (number === 0) {
@@ -11,31 +13,40 @@ $(function () {
     } else if (/0.*/.test(number)) {
       returnedValue = "BEEP";
     } else {
-      returnedValue = number;
+      returnedValue = number.toString();
     }
     return returnedValue;
   }
 
-
-
   $("form#beep-form").submit(function(event) {
     event.preventDefault();
+    $("#output").html("");
 
     var output = "";
-    // var animator = 0;
-    // var speed = 50;
+    var animator = 0;
+    var speed = 20;
     var userInput = parseInt($("input#userInput").val());
+    memoryBanks.push(userInput);
     output = numCheck(userInput);
 
-    // function printOut(text) {
-    //   if (animator < text.length) {
-    //     $("span#output").append(text[animator]);
-    //     animator++;
-    //     setTimeout(printOut, speed);
-    //   }
-    // }
-    // printOut(output);
-    $("#output").text(output);
+    function printOut() {
+      if (animator < output.length) {
+        $("#output").append(output.charAt(animator));
+        animator++;
+        setTimeout(printOut, speed);
+      }
+    }
+    printOut(output);
+  });
+
+  $("button#full-print").click(function() {
+    var total= 0;
+    memoryBanks.forEach(function(value) {
+      total = total + parseInt(value);
+    });
+    var printMemory = memoryBanks.join(" ");
+    $("#output").html("<p>" + printMemory + "</p>");
+    $("#output").append("<p> Total: " + total.toString() + "</p>");
   });
 
 });
