66be7359dca5657fca0f73a6976b03e68e7bdc37
Dan Mace <dmacebeta@gmail.com>
2018-08-17 12:10:30 -0700

    working on printout animation

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,10 @@ $(function () {
 // This numCheck function satisfies the requirements of the project.  It displays an understanding of branches, operators and RegEx, but this week was about Arrays, and while splitting the number into individual digits and then checking each digit for the 0 || 1 condition is tempting, the code below is far better performant.  So, I'll hold on to this function cause I like it's simplicity, but to make sure you know I can do the array thing, I'll still dump the values into an array and print them back out to the user.  A back-end window, if you will.
   var numCheck = function(number) {
     var returnedValue = "";
-    if (number%3 === 0){
-      returnedValue = "I'm sorry, Dave. I'm afraid I can't do that.";
+    if (number === 0) {
+    returnedValue = "I'm sorry, Dave. I'm afraid I can't do that. You cannot divide the infinite nothing.";
+    } else if (number%3 === 0){
+      returnedValue = "I'm sorry, Dave. I'm afraid I can't do that. I have not been programmed to process multiples of three.";
     } else if (/1.*/.test(number)) {
       returnedValue = "BOOP";
     } else if (/0.*/.test(number)) {
@@ -14,13 +16,25 @@ $(function () {
     return returnedValue;
   }
 
+
+
   $("form#beep-form").submit(function(event) {
     event.preventDefault();
+    $("span#output").html("");
     var output = "";
+    var animator = 0;
+    var speed = 50;
     var userInput = parseInt($("input#userInput").val());
     output = numCheck(userInput);
 
-    $("#output").text(output);
+    function printOut(text) {
+      if (animator < text.length) {
+        $("span#output").append(text[animator]);
+        animator++;
+        setTimeout(printOut, speed);
+      }
+    }
+    printOut(output);
 
   });
 
