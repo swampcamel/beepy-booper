8714d56b957a77522d8b09f66df9aed29d94ea94
Dan Mace <dmacebeta@gmail.com>
2018-08-23 20:58:04 -0700

    refactoring and seperation of business and interface logic, with some small min-width css changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,57 @@
+// Back End Logic
+
+var beep = new Audio('media/bleep_01.wav');
+var boop = new Audio('media/bleep_05.wav');
+var sorry = new Audio('media/cantdo.wav');
+
+var memoryBanks = [];
+
+function totalScreen() {
+  var total = 0;
+  memoryBanks.forEach(function(value, index) {
+   var num = parseInt(value);
+
+   if (num === num) {
+     total = total + num;
+
+   } else {
+     memoryBanks.splice(index, 1);
+   }
+ });
+ return total;
+}
+
+var numCheck = function(number) {
+  var numberArray = [];
+
+  for (var j=0; j <= number; j++) {
+    numberArray.push(j);
+
+    if ((numberArray[j] === 0) || (numberArray[j]%3 === 0)) {
+      numberArray[j] = "I'm sorry, Dave.";
+
+    } else if (/1.*/.test(numberArray[j])) {
+      numberArray[j] = "BOOP";
+
+    } else if (/0.*/.test(numberArray[j])) {
+      numberArray[j] = "BEEP";
+    }
+  }
+
+  var newReturn = numberArray.join(" ");
+  return newReturn;
+}
+
+var reverseString = function(str) {
+  var reversedString = str.split("");
+  reversedString = reversedString.reverse();
+  reversedString = reversedString.join("");
+  return reversedString;
+}
+
+// Interface Logic
+
 $(function () {
-  var memoryBanks = [];
-  var beep = new Audio('media/bleep_01.wav');
-  var boop = new Audio('media/bleep_05.wav');
-  var sorry = new Audio('media/cantdo.wav');
-
-  var numCheck = function(number) {
-    // var returnedValue = "";
-    var numberArray = [];
-    for (var i = 0; i < number+1; i++) {
-      numberArray.push(i);
-    }
-    for (var j=0; j < numberArray.length; j++) {
-      if ((numberArray[j] === 0) || (numberArray[j]%3 === 0)) {
-        numberArray[j] = "I'm sorry, Dave.";
-      } else if (/1.*/.test(numberArray[j])) {
-        numberArray[j] = "BOOP";
-      } else if (/0.*/.test(numberArray[j])) {
-        numberArray[j] = "BEEP";
-      }
-    }
-    console.log(numberArray);
-    if (number === 0) {
-      sorry.play();
-      // returnedValue = "I'm sorry, Dave. I'm afraid I can't do that. You cannot divide the infinite nothing.";
-    } else if (number%3 === 0){
-      sorry.play();
-      // returnedValue = "I'm sorry, Dave. I'm afraid I can't do that. I have not been programmed to process multiples of three.";
-
-    } else if (/1.*/.test(number)) {
-      $("#title").addClass("shake-horizontal");
-      setTimeout(function() {
-        $("#title").removeClass("shake-horizontal");
-      }, 350);
-      boop.play();
-      // returnedValue = "BOOP";
-
-    } else if (/0.*/.test(number)) {
-      $("#title").addClass("shake-horizontal");
-      setTimeout(function() {
-        $("#title").removeClass("shake-horizontal");
-      }, 350);
-      beep.play();
-      // returnedValue = "BEEP";
-
-    } else {
-     console.log("No catches.");
-    }
-    var newReturn = numberArray.join(" ");
-    return newReturn;
-  }
-
-  var reverseString = function(str) {
-    var reversedString = str.split("");
-    reversedString = reversedString.reverse();
-    reversedString = reversedString.join("");
-    return reversedString;
-  }
-
   $("form#beep-form").submit(function(event) {
     event.preventDefault();
     $("#output").html("");
@@ -65,10 +60,26 @@ $(function () {
     var animator = 0;
     var speed = 20;
     var userInput = parseInt($("input#userInput").val());
-
     memoryBanks.push(userInput);
 
+    if (userInput === 0 || userInput%3 === 0) {
+      sorry.play();
 
+    } else if (/1.*/.test(userInput)) {
+      $("#title").addClass("shake-horizontal");
+      setTimeout(function() {
+        $("#title").removeClass("shake-horizontal");
+      }, 350);
+      boop.play();
+
+    } else if (/0.*/.test(userInput)) {
+      $("#title").addClass("shake-horizontal");
+      setTimeout(function() {
+        $("#title").removeClass("shake-horizontal");
+      }, 350);
+      beep.play();
+    }
+    // wasn't able to move this outside of submit because of scope issues with the input trying to be passed from the submit function to another function outside it
     function printOut() {
       if (animator < output.length) {
         $("#output").append(output.charAt(animator));
@@ -88,22 +99,9 @@ $(function () {
   });
 
   $("button#full-print").click(function() {
-    var total= 0;
+    var total = totalScreen();
 
-    memoryBanks.forEach(function(value, index) {
-      var num = parseInt(value);
-      if (num === num) {
-      console.log(num);
-      total = total + num;
-    } else {
-      memoryBanks.splice(index, 1);
-      console.log(num);
-    }
-    });
-
-    var printMemory = memoryBanks.join(" ");
-
-    $("#output").html("<p>" + printMemory + "</p>");
+    $("#output").html("<p>" + memoryBanks.join(" ") + "</p>");
     $("#output").append("<p> Total: " + total.toString() + "</p>");
   });
 
